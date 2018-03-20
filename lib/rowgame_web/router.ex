1bd3660dadff407c0222c99993ddd053626d88e4
MBlaureNs <42chrono@gmail.com>
2018-03-20 17:37:19 +0000

    bugfix router

--- lib/rowgame_web/router.ex
+++ lib/rowgame_web/router.ex
@@ -7,9 +7,6 @@ defmodule RowgameWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-
-    resources "/users", RowgameWeb.UserController
-    resources "/games", RowgameWeb.GameController
   end
 
   pipeline :api do
@@ -20,6 +17,8 @@ defmodule RowgameWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/users", UserController
+    resources "/games", GameController
   end
 
   # Other scopes may use custom stacks.
