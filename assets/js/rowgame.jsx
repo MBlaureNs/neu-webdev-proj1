65c299fd386e7832b5c11b28d662131f5960f0bc
pahalsinghania <pahalsinghania@gmail.com>
2018-03-23 14:12:16 -0400

    elixir connected

--- assets/js/rowgame.jsx
+++ assets/js/rowgame.jsx
@@ -14,9 +14,7 @@ class Demo extends React.Component {
                 this.channel.join()
                   .receive("ok", this.gotView.bind(this))
                   .receive("error", resp => {console.log("Unable to join", resp)});
-	}
   }
-
 
   gotView(view) {
     this.setState(view.game);
