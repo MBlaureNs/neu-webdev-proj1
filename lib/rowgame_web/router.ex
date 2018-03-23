e45451a320202beff9a83bb8fe08f431e8d9d1d4
MBlaureNs <42chrono@gmail.com>
2018-03-23 16:59:21 +0000

    user login

--- lib/rowgame_web/router.ex
+++ lib/rowgame_web/router.ex
@@ -4,6 +4,7 @@ defmodule RowgameWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
+    plug :get_current_user
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
@@ -13,12 +14,24 @@ defmodule RowgameWeb.Router do
     plug :accepts, ["json"]    
   end
 
+  # adapted from nat's notes
+  # http://www.ccs.neu.edu/home/ntuck/courses/2018/01/cs4550/notes/12-microblog/notes.html
+  def get_current_user(conn, _params) do
+    # TODO: Move this function out of the router module.
+    user_id = get_session(conn, :user_id)
+    user = Rowgame.Accounts.get_user(user_id || -1)
+    assign(conn, :current_user, user)
+  end
+
   scope "/", RowgameWeb do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
     resources "/users", UserController
     resources "/games", GameController
+
+    post "/session", SessionController, :create
+    delete "/session", SessionController, :delete
   end
 
   scope "/api/v1", RowgameWeb do
