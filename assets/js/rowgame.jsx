f533e8ee0eddf312608c3fce514e946d0fb1e6bf
MBlaureNs <42chrono@gmail.com>
2018-03-27 23:36:49 +0000

    turn sequence

--- assets/js/rowgame.jsx
+++ assets/js/rowgame.jsx
@@ -33,7 +33,7 @@ class Demo extends React.Component {
       "winner_id": 0,
       "move": [],
       "chat": [],
-      "board": [["empty"]]
+      "board": [[]]
     };
     //console.log("def state", s)
     return s;
@@ -67,8 +67,23 @@ class Demo extends React.Component {
     //console.log(this.state);
   }
 
+  isHost() {
+    return this.state.host_id == window.current_user_id; 
+  }
+
+  isClient() {
+    return this.state.client_id == window.current_user_id;
+  }
+
+  isSpec() {
+    return !this.isHost() && !this.isClient();
+  }
+  
   isMyTurn() {
-    return (this.state.cur_turn % 2 == 0) == (this.state.host == window.current_user_id);
+    return !this.state.is_finished &&
+      this.state.is_started &&
+      !this.isSpec() &&
+      ((this.state.cur_turn % 2 == 1) == this.isHost());
   }
   
   handleClick(x,y) {
@@ -83,7 +98,7 @@ class Demo extends React.Component {
   }
 
   handleJoin() {
-    this.channel.push("join", current_user)
+    this.channel.push("join", {"client": window.current_user_id})
 	.receive("ok", this.gotView.bind(this));
   }
   
@@ -96,6 +111,7 @@ class Demo extends React.Component {
         onClick = {() => this.handleClick(x,y)}
 	x = {x}
 	y = {y}
+	isMyTurn = {this.isMyTurn()}
       />
     );
   }
@@ -111,12 +127,154 @@ class Demo extends React.Component {
       board.push(x);
     }
     return (
-      <div>
+      <div className="container">
+	<RuleBar
+	   win_length={this.state.win_length}
+	   />
+	<StatusBar
+	   turn={this.state.cur_turn}
+	   start={this.state.is_started}
+	   finish={this.state.is_finished}
+	   host_id={this.state.host_id}
+	   host_name={this.state.host_name}
+	   client_id={this.state.client_id}
+	   client_name={this.state.client_name}
+	   winner_id={this.state.winner_id}
+	   isHost={this.isHost()}
+	   isClient={this.isClient()}
+	   isSpec={this.isSpec()}
+	   start_onClick={() => this.handleStart()}
+	   join_onClick={() => this.handleJoin()}
+	   />
 	{board}
       </div>
     );
   }
 }
+
+function RuleBar(params) {
+  var p = params;
+  return (
+    <div id="rule-bar" className="row">
+      {p.win_length} in a row to win
+    </div>
+  );
+}
+
+//this is the worst code i've written
+function StatusBar(params) {
+  var p = params;
+
+  if (p.finish) {  //game finished
+    if (p.winner_id == p.host_id) { //host won
+      return (
+	<div id="status-bar" className="row">
+	  Game over<br/>
+	  {p.host_name} (B) vs. {p.client_name} (W)<br/>
+	  Winner: {p.host_name}
+	</div>
+      );
+    } else { //p.winner_id == p.client_id, client won
+      return (
+	<div id="status-bar" className="row">
+	  Game over<br/>
+	  {p.host_name} (B) vs. {p.client_name} (W)<br/>
+	  Winner: {p.client_name}
+	</div>
+      );
+    }
+  } else if (p.start) { //game in progress
+    var whoseTurn = "";
+    if (p.turn % 2 == 1) {
+      whoseTurn = p.host_name + "'s turn";
+    } else {
+      whoseTurn = p.client_name + "'s turn";
+    }
+    if (p.isSpec) {
+      return (
+	<div id="status-bar" className="row">
+	  You are spectating:<br/>
+	  {p.host_name} (B) vs. {p.client_name} (W)<br/>
+	  Turn: {p.turn}<br/>
+	  {whoseTurn}<br/>
+	</div>
+      );
+    } else if (p.isClient) {
+      return (
+	<div id="status-bar" className="row">
+	  You are client:<br/>
+	  {p.host_name} (B) vs. {p.client_name} (W)<br/>
+	  Turn: {p.turn}<br/>
+	  {whoseTurn}<br/>
+	</div>
+      );
+    } else { //p.isHost
+      return (
+	<div id="status-bar" className="row">
+	  You are host:<br/>
+	  {p.host_name} (B) vs. {p.client_name} (W)<br/>
+	  Turn: {p.turn}<br/>
+	  {whoseTurn}<br/>
+	</div>
+      );
+    }
+  } else if (p.client_id) { // client joined
+    if (p.isSpec) {
+      return (
+	<div id="status-bar" className="row">
+	  You are spectating:<br/>
+	  {p.host_name} (B) vs. {p.client_name} (W)<br/>
+	  Waiting for host to start...
+	</div>
+      );
+    } else if (p.isClient) {
+      return (
+	<div id="status-bar" className="row">
+	  You are client:<br/>
+	  {p.host_name} (B) vs. {p.client_name} (W)<br/>
+	  Waiting for host to start...
+	</div>
+      );
+    } else { //p.isHost
+      return (
+	<div id="status-bar" className="row">
+	  You are host:<br/>
+	  {p.host_name} (B) vs. {p.client_name} (W)<br/>
+	  <button id="start-button" onClick={p.start_onClick}>Start game</button>
+	</div>
+      );
+    }
+  } else { // waiting for client
+    if (p.isSpec) {
+      if (window.current_user_id == -1) {
+	return (
+	  <div id="status-bar" className="row">
+	    You are spectating:<br/>
+	    {p.host_name} (B) vs. [waiting for player] (W)<br/>
+	    <a href="/">Log in to join games</a>
+	  </div>
+	);
+      } else {
+	  return (
+	    <div id="status-bar" className="row">
+	      You are spectating:<br/>
+	      {p.host_name} (B) vs. [waiting for player] (W)<br/>
+	      <button id="start-button" onClick={p.join_onClick}>Join game</button>
+	    </div>
+	  );
+      }
+    } else { //p.isHost
+      return (
+	<div id="status-bar" className="row">
+	  You are host:<br/>
+	  {p.host_name} (B) vs. [waiting for player] (W)<br/>
+	  Waiting for client...
+	</div>
+      );
+    }
+  }
+}
+
 
 function Tile(params) {
   var p = params;
@@ -126,6 +284,10 @@ function Tile(params) {
   } else if (p.state == "black") {
     return (<div id={div_id} className="tile tile-black">b</div>);
   } else { //p.state = "empty"
-    return (<div id={div_id} className="tile tile-empty" onClick={params.onClick}/>);
+    if (p.isMyTurn) {
+      return (<div id={div_id} className="tile tile-empty" onClick={params.onClick}/>);
+    } else {
+      return (<div id={div_id} className="tile tile-empty"/>);
+    }
   }
 }
