b0ed64aaef4a763fbe5d7b9f6a47a36a76fef37b
MBlaureNs <42chrono@gmail.com>
2018-03-27 01:01:51 +0000

    broadcast

--- assets/js/rowgame.jsx
+++ assets/js/rowgame.jsx
@@ -13,8 +13,9 @@ class Demo extends React.Component {
     this.channel = props.channel;
     
     this.channel.join()
-      .receive("ok", this.gotView.bind(this))
-      .receive("error", resp => {console.log("Unable to join", resp)});
+	.receive("ok", this.gotView.bind(this))
+	.receive("error", resp => {console.log("Unable to join", resp)});
+    this.channel.on("update", this.gotView.bind(this));
   }
 
   defaultState() {
